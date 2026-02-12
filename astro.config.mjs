95629b956e0ce8b2509535cf22372c2799fcb480
Sangwon Ryu <me@sryu.dev>
2026-02-12 13:58:01 +0900

    feat: add navbar and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [icon(), solidJs()]
+});
